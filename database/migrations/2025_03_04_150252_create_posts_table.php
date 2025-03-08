5e5eca3f341d0280e169d317e5b1b25d36241389
Plinphon Patthayawit <plinphon@Plinphons-MacBook-Pro.local>
2025-03-08 13:33:57 +0700

    create models & migration file for system and implement posts migration

---
+++ database/migrations/2025_03_04_150252_create_posts_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->string('topic');
+            $table->integer('user_id')->constrained();
+            $table->string('detail');
+            $table->timestamps('created_at');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('posts');
+    }
+};
